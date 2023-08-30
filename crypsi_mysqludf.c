88b3dc2e04a9b464755d1e3a813470ec4ad8f48f
wuriyanto <wuriyanto48@yahoo.co.id>
2023-08-30 10:19:22 +0700

    add aes 128 decrypt

--- crypsi_mysqludf.c
+++ crypsi_mysqludf.c
@@ -6,14 +6,21 @@
 extern "C" {
 #endif
 
+// AES 128 GCM encrypt
 my_bool mcrypsi_aes_128_gcm_encrypt_init(UDF_INIT* initid, UDF_ARGS* args, char* message);
 void mcrypsi_aes_128_gcm_encrypt_deinit(UDF_INIT* initid);
 char* mcrypsi_aes_128_gcm_encrypt(UDF_INIT* initid, UDF_ARGS* args, char* result, unsigned long* length, char* is_null, char* error);
+
+// AES 128 GCM decrypt
+my_bool mcrypsi_aes_128_gcm_decrypt_init(UDF_INIT* initid, UDF_ARGS* args, char* message);
+void mcrypsi_aes_128_gcm_decrypt_deinit(UDF_INIT* initid);
+char* mcrypsi_aes_128_gcm_decrypt(UDF_INIT* initid, UDF_ARGS* args, char* result, unsigned long* length, char* is_null, char* error);
 
 #ifdef __cplusplus
 }
 #endif
 
+// AES 128 GCM encrypt
 my_bool mcrypsi_aes_128_gcm_encrypt_init(UDF_INIT* initid, UDF_ARGS* args, char* message) {
     if (args->arg_count != 2) {
         strcpy(message, "mcrypsi_aes_128_gcm_encrypt requires key and text parameters");
@@ -54,6 +61,57 @@ char* mcrypsi_aes_128_gcm_encrypt(UDF_INIT* initid, UDF_ARGS* args, char* result
     ret = crypsi_aes_128_gcm_encrypt(input_key, input_text, text_size, &dst, &dst_size);
     if (ret != 0) {
         strcpy(error, "error encrypt with crypsi_aes_128_gcm_encrypt");
+		*is_null = 1;
+		return NULL;
+    }
+
+    *length = dst_size;
+    memcpy(result, dst, dst_size);
+
+    return result;
+}
+
+// AES 128 GCM decrypt
+my_bool mcrypsi_aes_128_gcm_decrypt_init(UDF_INIT* initid, UDF_ARGS* args, char* message) {
+    if (args->arg_count != 2) {
+        strcpy(message, "mcrypsi_aes_128_gcm_decrypt requires key and text parameters");
+        return 1;
+    }
+
+    if (args->arg_type[0] != STRING_RESULT || args->arg_type[1] != STRING_RESULT) {
+        strcpy(message, "mcrypsi_aes_128_gcm_decrypt requires key string and text string parameters");
+        return 1;
+    }
+
+    if (args->lengths[0] <= 0 || args->lengths[1] <= 0) {
+        strcpy(message, "mcrypsi_aes_128_gcm_decrypt parameters length value cannot less than 0");
+        return 1;
+    }
+
+    unsigned char* dst = NULL;
+
+    initid->ptr = (char*) dst;
+
+    return 0;
+}
+
+void mcrypsi_aes_128_gcm_decrypt_deinit(UDF_INIT* initid) {
+    if (initid->ptr != NULL) {
+        free((void*) initid->ptr);
+    }
+}
+
+char* mcrypsi_aes_128_gcm_decrypt(UDF_INIT* initid, UDF_ARGS* args, char* result, unsigned long* length, char* is_null, char* error) {
+    unsigned char* dst = (unsigned char*) initid->ptr;
+    int ret = 0;
+    char* input_key = args->args[0];
+    char* input_text = args->args[1];
+    int text_size = args->lengths[1];
+
+    int dst_size = 0;
+    ret = crypsi_aes_128_gcm_decrypt(input_key, input_text, text_size, &dst, &dst_size);
+    if (ret != 0) {
+        strcpy(error, "error encrypt with mcrypsi_aes_128_gcm_decrypt");
 		*is_null = 1;
 		return NULL;
     }
